102f36f33c9aec47c8f806f3fd8828db38a67fdd
Suchita <jsuchits923@gmail.com>
2026-08-15 00:09:47 +0530

    Music Player Website

---
+++ project.js
@@ -0,0 +1,24 @@
+function homeClick() 
+{
+    alert("Home clicked");
+}
+
+function openSearch() {
+    alert("Search clicked");
+}
+
+function openVideo() {
+    alert("Video clicked");
+}
+
+function closeApp() {
+    alert("Close clicked");
+}
+
+function openCharts() {
+    alert("Charts clicked");
+}
+
+function openSettings() {
+    alert("Settings clicked");
+}
